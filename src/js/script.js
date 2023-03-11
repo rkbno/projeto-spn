f55d5d55d1403d31bc5aab60a7eb0e1b6b15d215
rk <103226779+rkbno@users.noreply.github.com>
2023-03-11 19:21:20 -0300

    ajustes

--- src/js/script.js
+++ src/js/script.js
@@ -8,31 +8,32 @@ let clique5 = document.getElementById("botao5")
 let clique6 = document.getElementById("botao6")
 
 clique1.addEventListener("click", function (){
-    this.style.backgroundColor = "#1d8346"
+    
+    
     image.src= "./src/imagens/deam_sam1.jpg"
 })
 
 clique2.addEventListener("click", function (){
-    this.style.backgroundColor = "#1d8346"
+    
     image.src= "./src/imagens/deam_sam2.jpg"
 })
 
 clique3.addEventListener("click", function (){
-    this.style.backgroundColor = "#1d8346"
+    
     image.src= "./src/imagens/deam_sam3.jpg"
 })
 
 clique4.addEventListener("click", function (){
-    this.style.backgroundColor = "#1d8346"
+    
     image.src= "./src/imagens/deam_sam4.jpg"
 })
 
 clique5.addEventListener("click", function (){
-    this.style.backgroundColor = "#1d8346"
+    
     image.src= "./src/imagens/deam_sam5.jpg"
 })
 
 clique6.addEventListener("click", function (){
-    this.style.backgroundColor = "#1d8346"
+    
     image.src= "./src/imagens/deam_sam6.jpg"
 })
