2550aeadf2094a8bb67c7f96f8400250beb400cf
rk <103226779+rkbno@users.noreply.github.com>
2023-03-11 19:10:57 -0300

    ajustes

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+let image = document.getElementById("like")
+
+let clique1 = document.getElementById("botao1")
+let clique2 = document.getElementById("botao2")
+let clique3 = document.getElementById("botao3")
+let clique4 = document.getElementById("botao4")
+let clique5 = document.getElementById("botao5")
+let clique6 = document.getElementById("botao6")
+
+clique1.addEventListener("click", function (){
+    this.style.backgroundColor = "#1d8346"
+    image.src= "./src/imagens/deam_sam1.jpg"
+})
+
+clique2.addEventListener("click", function (){
+    this.style.backgroundColor = "#1d8346"
+    image.src= "./src/imagens/deam_sam2.jpg"
+})
+
+clique3.addEventListener("click", function (){
+    this.style.backgroundColor = "#1d8346"
+    image.src= "./src/imagens/deam_sam3.jpg"
+})
+
+clique4.addEventListener("click", function (){
+    this.style.backgroundColor = "#1d8346"
+    image.src= "./src/imagens/deam_sam4.jpg"
+})
+
+clique5.addEventListener("click", function (){
+    this.style.backgroundColor = "#1d8346"
+    image.src= "./src/imagens/deam_sam5.jpg"
+})
+
+clique6.addEventListener("click", function (){
+    this.style.backgroundColor = "#1d8346"
+    image.src= "./src/imagens/deam_sam6.jpg"
+})
