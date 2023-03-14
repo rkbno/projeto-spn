f3742c77f88db2381144fa20f3e4d948ec063c86
rk <103226779+rkbno@users.noreply.github.com>
2023-03-13 22:49:22 -0300

    acabei

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,42 @@
-let image = document.getElementById("like")
 
-let clique1 = document.getElementById("botao1")
-let clique2 = document.getElementById("botao2")
-let clique3 = document.getElementById("botao3")
-let clique4 = document.getElementById("botao4")
-let clique5 = document.getElementById("botao5")
-let clique6 = document.getElementById("botao6")
+// passo 1: dar um jeito de pegar o elemento HTML dos botões 
+const botoesCarossel = document.querySelectorAll('.botao')
+const imagens = document.querySelectorAll('.imagem')
 
-clique1.addEventListener("click", function (){
-    
-    
-    image.src= "./src/imagens/deam_sam1.jpg"
+// passo 2: dar um jeito de identificar o clique do usuário no botão 
+botoesCarossel.forEach((botao, i) =>  {
+    botao.addEventListener('click', function () {
+        // passo 3: desmarcar o botão selecionado no anterior
+        desativarBotaoSelecionado()
+        // passo 4: marcar o botão clicado como se estivesse selecionado 
+        marcarBotaoSelecionado(botao)
+        // passo 5: esconder a imagem de fundo anterior
+        esconderImagemAtiva()
+        // passo 6: fazer aparecer a imagem de fundo correspondente ao botão clicado
+        mostrarImagemFundo(i)
+        
+    })
 })
 
-clique2.addEventListener("click", function (){
-    
-    image.src= "./src/imagens/deam_sam2.jpg"
-})
 
-clique3.addEventListener("click", function (){
-    
-    image.src= "./src/imagens/deam_sam3.jpg"
-})
 
-clique4.addEventListener("click", function (){
-    
-    image.src= "./src/imagens/deam_sam4.jpg"
-})
 
-clique5.addEventListener("click", function (){
-    
-    image.src= "./src/imagens/deam_sam5.jpg"
-})
 
-clique6.addEventListener("click", function (){
-    
-    image.src= "./src/imagens/deam_sam6.jpg"
-})
+function mostrarImagemFundo(i) {
+    imagens[i].classList.add('ativa')
+}
+
+function marcarBotaoSelecionado(botao) {
+    botao.classList.add('selecionado')
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa')
+    imagemAtiva.classList.remove('ativa')
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado')
+    botaoSelecionado.classList.remove('selecionado')
+}
+
